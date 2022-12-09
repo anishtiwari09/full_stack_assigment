e28d42a086bf5cf58c24054a53f20e47da518cec
Anish Tewari <aneeshtiwari24@gmail.com>
2022-12-10 03:38:51 +0530

    add ssl

--- backend/playquiz/index.js
+++ backend/playquiz/index.js
@@ -1,5 +1,8 @@
 const PORT=5555
 const express=require('express');
+const https=require('https')
+const fs=require('fs')
+const path=require('path')
 const connect = require('./Config/db.config');
 const router=require('./router')
 const cors=require('cors')
@@ -10,8 +13,14 @@ app.use(express.json())
 app.use('/',router);
 (async () => {
     await connect()
-    app.listen(PORT, () => {
+    // app.listen(PORT, () => {
 
-        console.log("Connected at " + PORT)
-    })
+    //     console.log("Connected at " + PORT)
+    // })
+    const sslServer=https.createServer({
+        key:fs.readFileSync(path.join(__dirname,'key.pem')),
+        cert:fs.readFileSync(path.join(__dirname,'cert.pem'))
+    },app);
+    sslServer.listen(PORT,()=>console.log("connected at "+PORT))
+
 })();
